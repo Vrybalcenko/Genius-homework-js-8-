6a9703bd09e9da17520a68ebd00c1522061d4847
Vrybalcenko <165685927+Vrybalcenko@users.noreply.github.com>
2025-01-20 01:16:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// Домашнє завдання
+// -----1-----
+// Напишіть код, щоб вибрати елемент з атрибутом data-widget-name з документа
+// та прочитати його значення.
+// <!DOCTYPE html>
+// <html>
+// <body>
+// <div data-widget-name="menu">Виберіть жанр</div>
+// <script>/* ваш код */</script>
+// </body>
+// </html>
+
+// let elem = document.querySelector("[data-widget-name]");
+
+// alert(elem.getAttribute("data-widget-name"));
+
